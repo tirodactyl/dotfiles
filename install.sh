b3da5d1554acf6b72a71ee53e4694eaf50f2f4d3
Tirso Peguero <tito.peguero@gmail.com>
2018-05-04 00:26:41 -0700

    make install work

--- install.sh
+++ install.sh
@@ -3,7 +3,24 @@ set -euo pipefail
 IFS=$'\n\t'
 # The above lines found in this article: https://dev.to/thiht/shell-scripts-matter
 
+current_dir=$(dirname $0)
+dotfiles_path=$PWD/$current_dir
+# vimfiles_path=$dotfiles_path/vimfiles
+
 echo "Installing dotfiles to local system..."
 
-echo "Symlinking files to $HOME..."
-ln -s tmux.conf $HOME/.tmux.conf
+#took some stuff from here...
+
+echo "=== Creating symlinks for dotfiles"
+cd $dotfiles_path
+for name in symlink/*; do
+  name=$(echo $name|cut -c 9-)
+  target=$HOME/.$name
+
+  if [ -e $target ]; then
+    echo "WARNING: $target exists but not a symlink" && continue
+  else
+    ln -s $dotfiles_path/symlink/$name $HOME/.$name
+  fi
+done
+cd -
