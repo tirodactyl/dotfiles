17ce832bd11daa774e94509514d2ee38ccf7e2f2
Tirso Peguero <tito.peguero@gmail.com>
2018-09-10 16:34:25 -0700

    move karabiner into config and alter setup script to link those files as well

--- install.sh
+++ install.sh
@@ -11,7 +11,8 @@ echo "Installing dotfiles to local system..."
 
 #took some stuff from here...
 
-echo "=== Creating symlinks for dotfiles"
+# symlink true dotfiles into ~/
+echo "=== Creating symlinks for dotfiles in $HOME/"
 cd $dotfiles_path
 for name in symlink/*; do
   name=$(echo $name|cut -c 9-)
@@ -20,9 +21,26 @@ for name in symlink/*; do
   if [ -e $target ]; then
     echo "WARNING: $target exists but not a symlink" && continue
   else
-    ln -s $dotfiles_path/symlink/$name $HOME/.$name
+    ln -s $dotfiles_path/symlink/$name $target
+    echo "- linked to $target"
   fi
 done
+
+# symlink into .config
+echo "=== Creating symlinks for other configuration files in $HOME/.config/"
+for name in config/*; do
+  name=$(echo $name|cut -c 9-)
+  target=$HOME/.config/$name
+
+  if [ -e $target ]; then
+    echo "WARNING: $target exists but not a symlink" && continue
+  else
+    ln -s $dotfiles_path/config/$name $target
+    echo "- linked to $target"
+  fi
+done
+
+# go back to where you came from
 cd -
 
 # this makes finder show hidden files by default
