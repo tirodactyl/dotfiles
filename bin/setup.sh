2cbccdda2981f0deff5afa60aba33c7bde3b96b9
Tirso Peguero <tito.peguero@gmail.com>
2018-09-14 12:52:20 -0700

    remove dir creation cause brew creates them already

--- bin/setup.sh
+++ bin/setup.sh
@@ -20,10 +20,6 @@ cd $HOME/dev/tirodactyl/dotfiles && git pull
 echo "\n Installing Homebrew"
 # see: github.com/kickstarter/laptop/pull/49 & github.com/Homebrew/brew/issues/3228
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-sudo mkdir /usr/local/Cellar
-sudo mkdir /usr/local/Frameworks
-sudo mkdir /usr/local/include
-sudo mkdir /usr/local/opt
 sudo chown -R $(whoami) $(brew --prefix)/*
 
 echo "\n Installing homebrew dependencies"
