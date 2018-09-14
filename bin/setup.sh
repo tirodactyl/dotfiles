e30af01b7ac7607970644ccbab3eca302f514f32
Tirso Peguero <tito.peguero@gmail.com>
2018-09-14 12:36:36 -0700

    reorder to enable git via xcode dev tools

--- bin/setup.sh
+++ bin/setup.sh
@@ -12,13 +12,13 @@ sudo -v
 # Keep-alive: update existing `sudo` time stamp until script has finished
 while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
+echo "\n Installing XCode dev tools"
+xcode-select --install
+
 echo "Gathering dependencies"
 mkdir -p $HOME/dev/tirodactyl
 git clone https://github.com/tirodactyl/dotfiles.git $HOME/dev/tirodactyl/dotfiles
 cd $HOME/dev/tirodactyl/dotfiles && git pull
-
-echo "\n Installing XCode dev tools"
-xcode-select --install
 
 echo "\n Installing Homebrew"
 sudo chown -R $USER /usr/local
